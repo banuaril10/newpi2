6a24e49916445dc7bca7a879ff0cbc74c410d66a
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-12-15 09:50:29 +0700

    12-12-2023

--- mitemspromocode_live.php
+++ mitemspromocode_live.php
@@ -214,10 +214,12 @@
 						$no = 1;
 						foreach ($arrs as $row) { 
 						$name = "";
-						$q_cek_name = "select name from pos_mproduct where sku = '".$row['sku']."' ";
+						$price = 0;
+						$q_cek_name = "select name, price from pos_mproduct where sku = '".$row['sku']."' ";
 								
 						foreach ($connec->query($q_cek_name) as $row1) {
 							$name = $row1['name'];
+							$price = $row1['price'];
 						}
 						
 						if($_GET['stock'] && !empty($_GET['stock'])){ 
@@ -234,7 +236,7 @@
 							
 						<?php if($q_stock > 0){ ?>
 							<tr>
-								<td><input type="checkbox" id="checkbox" name="checkbox[]" value="<?php echo $row['sku']; ?>|<?php echo $name; ?>|<?php echo $row['price1']; ?>|<?php echo $row['price2']; ?>|<?php echo $row['price3']; ?>|<?php echo date('Y-m-d'); ?>|<?php echo $row['rack']; ?>|<?php echo $row['todate']; ?>|"></td>
+								<td><input type="checkbox" id="checkbox" name="checkbox[]" value="<?php echo $row['sku']; ?>|<?php echo $name; ?>|<?php echo $row['price1']; ?>|<?php echo $row['price2']; ?>|<?php echo $row['price3']; ?>|<?php echo date('Y-m-d'); ?>|<?php echo $row['rack']; ?>|<?php echo $row['todate']; ?>|<?php echo $price; ?>"></td>
 								<td scope="row"><?php echo $no; ?></td>
 								<td><?php echo $row['sku']; ?></td>
 								<td><?php echo $name; ?></td>
@@ -254,7 +256,7 @@
 						<?php }else{ ?>
 							
 								<tr>
-								<td><input type="checkbox" id="checkbox" name="checkbox[]" value="<?php echo $row['sku']; ?>|<?php echo $name; ?>|<?php echo $row['price1']; ?>|<?php echo $row['price2']; ?>|<?php echo $row['price3']; ?>|<?php echo date('Y-m-d'); ?>|<?php echo $row['rack']; ?>|<?php echo $row['todate']; ?>|"></td>
+								<td><input type="checkbox" id="checkbox" name="checkbox[]" value="<?php echo $row['sku']; ?>|<?php echo $name; ?>|<?php echo $row['price1']; ?>|<?php echo $row['price2']; ?>|<?php echo $row['price3']; ?>|<?php echo date('Y-m-d'); ?>|<?php echo $row['rack']; ?>|<?php echo $row['todate']; ?>|<?php echo $price; ?>"></td>
 								<td scope="row"><?php echo $no; ?></td>
 								<td><?php echo $row['sku']; ?></td>
 								<td><?php echo $name; ?></td>
@@ -294,7 +296,7 @@
 </div>
 </div>
 
-
+<script src="styles/js/price-promo-tingkat.js"></script>
 <script type="text/javascript">
 $(document).ready( function () {
     $('#example').DataTable({
@@ -547,178 +549,6 @@ function formatRupiah(angka, prefix){
 			});
 			
 			
-			document.getElementById("btn-cetak-tinta").addEventListener("click", function() { //cetak besar
-				var array = [];
-				var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
-				
-				for (var i = 0; i < checkboxes.length; i++) {
-					array.push(checkboxes[i].value)
-				}
-				let text = "<table><tr>";
-				// let text = "<div style='position: relative; width: 920px; height: 135px; padding-top: 25px; border: 1px solid #000;'>";
-				
-				var x = 1;
-				for (let i = 0; i < array.length; i++) {
-	var res = array[i].split("|");
-						
-						if(res[5] === 'undefined' || res[5] === 'null' || res[5] === ''){
-							
-							var sc = '';
-						}else{
-							var sc = ' / '+res[5];
-						}
-						
-						// if(i == 0){
-							// text += "";
-						// }
-						if(x==5){
-							var x = 1;
-						}
-						
-						var lengthh = res[1].length;
-						var panjangharga = parseInt(res[6]);
-						
-						var br = "<br>";
-						
-						
-						
-						var sizeprice = "45px";
-						if(lengthh > 33){
-							
-							 sizeprice = "45px";
-						}
-						
-						if(panjangharga > 999999){
-							sizeprice = "40px";
-							
-						}
-						
-						if(res[4] != ""){
-							var rack = res[0]+"/"+res[4]+"/"+res[8];
-							
-							
-						}else{
-							
-							var rack = res[0]+"/NO_RACK/"+res[8];
-						}
-						
-						
-						
-						var newStr = rack.replace('-', '_');
-						// text += "<td style='border: 0.5px solid #000;'><div style='margin:5px 5px 0 10px; text-align:left; display: inline-block; color: black; width: 171px; height: 121px; font-family: Calibri'><table>"+
-						// "<tr><td style='padding:0; border: 1px'><label style='text-align: right; font-size: 14px'><b>"+res[1].toUpperCase()+"</b></label></td></tr>"+
-						// "<tr><td style='padding:0'><label style='text-align: left; font-size: 8px'><b>Rp </b></label><label style='text-align: left; font-size: 15px; text-decoration: line-through;'><b>"+formatRupiah(res[2], '')+"</b></label></td></tr>"+
-						// "<tr><td style='padding:0'><label style='text-align: left; font-size: 14px'><b>Rp </b></label><label style='text-align: left; font-size: 30px;'><b>"+formatRupiah(res[6], '')+"</b></label></td></tr>"+
-						// "<tr><td><label style='text-align: left; font-size: 14px'>"+res[0]+""+sc+"</label></td></tr>"+
-						// "<tr><td><label style='text-align: left; font-size: 14px'>"+res[3]+"</label> / <label style='text-align: left; font-size: 14px'>"+res[4].toUpperCase()+"</label></td></tr>"+
-						// "<tr><td><hr><label>HARGA KEJUTAN</label></td></tr></table></div></td>";
-						
-						text += "<td style='border: 0.5px solid #000'><div style='margin:5px 5px 0 5px; color: black; width: 177px; height: 121px; font-family: Calibri; '><div style='height:25px; text-align: left; font-size: 10px'><b>"+res[1].toUpperCase()+"</b></div><label style='text-align: left; font-size: 10px'><b>Rp </b></label><label style='text-align: left; font-size: 18px; text-decoration: line-through;'><b>"+formatRupiah(res[2], '')+"</b></label><label style='float: right !important; font-size: 10px;'> s.d. "+res[7]+"</label><label style='margin: -10px 0 0 0; float: right; font-size: "+sizeprice+"'><label style='font-size: 10px'><b>Rp </b></label><b>"+formatRupiah(res[6], '')+"</b></label> &nbsp &nbsp &nbsp &nbsp &nbsp <br><br><label style='text-align: left; font-size: 10px; width: 100%'>"+newStr+"</label><center><hr style='border-top: solid 1px #000 !important; background-color:black; border:none; height:1px; margin:1.5px 0 0 0;'><label style='text-align: center; font-size: 10px; margin-top: -10px'>HARGA KEJUTAN</label></center></div></td>";
-						
-						if((i+1)%4==0 && i!==0){
-							
-							text += "</tr><tr>";
-						}
-						x++;
-
-					}
-			
-				text += "</table>";
-					
-
-				  var mywindow = window.open('', 'my div', 'height=600,width=800');
-							/*optional stylesheet*/ //mywindow.document.write('<link rel="stylesheet" href="main.css" type="text/css" />');
-							mywindow.document.write('<style>@media print{@page {size: potrait; width: 216mm;height: 280mm;margin-top: 15;margin-right: 2;margin-left: 2; padding: 0;} margin: 0; padding: 0;} table { page-break-inside:auto }tr{ page-break-inside:avoid; page-break-after:auto }</style>');
-							mywindow.document.write(text);
-
-					
-							mywindow.print();
-							// mywindow.close();
-					
-							return true;
-			});
-			
-			document.getElementById("btn-cetak1-tinta").addEventListener("click", function() { //cetak besar
-				var array = [];
-				var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
-				
-				for (var i = 0; i < checkboxes.length; i++) {
-					array.push(checkboxes[i].value)
-				}
-				let text = "<table><tr>";
-				var x = 1;
-				for (let i = 0; i < array.length; i++) {
-						var res = array[i].split("|");
-						
-						if(res[5] === 'undefined' || res[5] === 'null'){
-							
-							var sc = '-';
-						}else{
-							var sc = res[5];
-						}
-						
-						// if(i == 0){
-							// text += "";
-						// }
-						if(x==5){
-							var x = 1;
-						}
-						
-						
-						
-						// var left1 = document.getElementById("left1").value;
-						// var left2 = document.getElementById("left2").value;
-						// var left3 = document.getElementById("left3").value;
-						// var left4 = document.getElementById("left4").value;
-						
-						// if(x==1){
-							// var left = left1;
-						// }else if(x==2){
-							// var left = left2;
-							
-						// }else if(x==3){
-							// var left = left3;
-							
-						// }else if(x==4){
-							// var left = left4;
-						// }
-
-
-						
-						text += "<td style='border: 1px solid #000;'><div style='margin:15px 0 0 20px; text-align:left; display: inline-block; color: black; width: 226px; height: 151px; font-family: Calibri'><label style='text-align: right; font-size: 10px'><b>"+res[1].toUpperCase()+"</b></label><br><label style='text-align: left; font-size: 10px'><b>Rp </b></label><label style='text-align: left; font-size: 18px'><b>"+formatRupiah(res[2], '')+"</b></label><br><label style='text-align: left; font-size: 10px'>"+res[0]+" | "+sc+"</label><br><label style='text-align: left; font-size: 10px'>"+res[3]+"</label> | <label style='text-align: left; font-size: 10px'>"+res[4].toUpperCase()+"</label></div></td>";
-						
-						if((i+1)%4==0 && i!==0){
-							
-							text += "</tr><tr>";
-						}
-						x++;
-				
-
-						// text += "<div style='text-align:left; left:"+left+"; position: absolute; display: inline-block; border: 1px; color: black; width: 202px; height: 151px; font-family: Calibri'><label style='text-align: right; font-size: 12px'>"+res[1].toUpperCase()+"</label><br><label style='text-align: left; font-size: 10px'><b>Rp </b></label><label style='text-align: left; font-size: 18px'><b>"+formatRupiah(res[2], '')+"</b></label><br><label style='text-align: left; font-size: 10px'>"+res[0]+"<br>"+sc+"</label><br><label style='text-align: left; font-size: 10px'>"+res[3]+"</label><br><label style='text-align: left; font-size: 10px'>"+res[4].toUpperCase()+"</label> </div>";
-						
-						// if((i+1)%4==0 && i!==0){
-							
-							// text += "</div><div style='position: relative;width: 920px; height: 151px; padding-top: 0'>";
-						// }
-						// x++;
-
-					}
-			
-				text += "</table>";
-					
-
-				  var mywindow = window.open('', 'my div', 'height=600,width=800');
-							/*optional stylesheet*/ //mywindow.document.write('<link rel="stylesheet" href="main.css" type="text/css" />');
-							mywindow.document.write('<style>@media print{@page {size: potrait; width: 216mm;height: 280mm;}}table { page-break-inside:auto }tr{ page-break-inside:avoid; page-break-after:auto }</style>');
-							mywindow.document.write(text);
-
-					
-							mywindow.print();
-							// mywindow.close();
-					
-							return true;
-			});
-
 			document.getElementById("checkall").addEventListener("click", function() {	
 
 				var checkboxes = document.querySelectorAll('input[type="checkbox"]');
