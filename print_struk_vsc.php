258a49594c303638cd12ad4574bd911f161ab89e
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-02-27 11:34:45 +0700

    cek omset

--- print_struk_vsc.php
+++ print_struk_vsc.php
@@ -20,71 +20,9 @@ $html = $_POST['html'];
 	$html = str_replace("       JL.","JL.", $html);
 	
 	for ($x = 0; $x <= 30; $x++) {
-	$html = str_replace("     ".$x." ", $x." ", $html);
+	$html = str_replace("        ".$x." ", $x." ", $html);
 	}
-	
-// $array_cat = array('SOFTLENS & AIR SOFTLENS',
-// 'ATK',
-// 'BABY CARE',
-// 'BATERAI && K. KADO',
-// 'BONEKA BULU',
-// 'BROSUR & PROMOSI LAINNYA',
-// 'BUKU BACAAN',
-// 'BUKU TULIS & SAMPUL',
-// 'CELENGAN',
-// 'PAYUNG & JAS HUJAN',
-// 'PERALATAN KOSMETIK',
-// 'ELEKTRONIK',
-// 'PERL. MAKAN',
-// 'STATIONARY 2',
-// 'MAINAN COWOK',
-// 'PERL. BAYI',
-// 'MAINAN CEWEK',
-// 'PLASTIK & LAINNYA',
-// 'KOSMETIK',
-// 'STATIONARY_AT',
-// 'FOOD & BEVERAGE',
-// 'E-COMMERCE',
-// 'HOME CARE',
-// 'KESEHATAN',
-// 'TAS SEKOLAH',
-// 'STREET WEAR',
-// 'AKSESORIS',
-// 'SENDAL',
-// 'MAINAN BAYI',
-// 'PERALATAN DAPUR & KEBERSIHAN',
-// 'ALAT MUSIK & OLAHRAGA',
-// 'NO SALE OPR',
-// 'FASHION BAG & WALLETS',
-// 'FASHION MUSLIM',
-// 'OBRAL',
-// 'PERKAKAS',
-// 'NO SALE MKT',
-// 'BD MOMMY CARE',
-// 'BD DIAPERS',
-// 'BD SUSU',
-// 'BD PERLENGKAPAN BAYI',
-// 'BD MOMS NEED',
-// 'BD TISSUE',
-// 'BD MAKANAN BAYI',
-// 'BD BABY CARE',
-// 'BD MAINAN BAYI',
-// 'BD PAKAIAN BAYI',
-// 'BD PAKAIAN ANAK',
-// 'BD BUKU EDUKASI ANAK',
-// 'BD MAINAN ANAK',
-// 'BD SANDAL DAN SEPATU',
-// 'BD BOTOL MINUM ANAK',
-// 'BD BONEKA BULU',
-// 'BD FANCY - GIFT',
-// 'BD HIJAB ANAK',
-// 'BD GIFT WRAPPING');
 
-
-// foreach ($array_cat as $x) {
-	// $html = str_replace($x."       ", $x, $html);
-// }
-	
 	$html = str_replace("TOTAL       ", "TOTAL", $html);
 	$html = str_replace("DISKON       ", "DISKON", $html);
 	$html = str_replace("GRAND TOTAL", "GRAND TOTAL", $html);
@@ -99,13 +37,18 @@ $html = $_POST['html'];
 	$html = str_replace("     IDOLMART Group   ", "IDOLMART Group", $html);
 	$html = str_replace("     #TERIMA KASIH#   ", "  #TERIMA KASIH#", $html);
 	$html = str_replace("     DITUKAR/DIKEMBALIKAN   ", "DITUKAR/DIKEMBALIKAN", $html);
+	$html = str_replace("BARANG YANG SUDAH DIBELI TIDAK DAPAT", "   BARANG YANG SUDAH DIBELI 
+          TIDAK DAPAT", $html);
 	$html = str_replace("=======================================", "================================", $html);
 	$html = str_replace("***************************************", "********************************", $html);
+	$html = str_replace("Nama Barang", "Nm Brg", $html);
+	$html = str_replace("Disc  ", "Disc", $html);
 	
 	
 // }
 
 $html = str_replace(" :         ", " : ", $html);
+$html = str_replace(" :    ", " : ", $html);
 
 $ip_printer = $_POST['ip_printer'];
 
