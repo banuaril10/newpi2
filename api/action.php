125d0fc13b12cb039d8912870626e332cf10a447
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-05-07 21:33:38 +0700

    2024-05-06 fix topic & case study

--- api/action.php
+++ api/action.php
@@ -1758,9 +1758,6 @@ if($_GET['modul'] == 'inventory'){
 							// $qtycount = $rrr['qtycount'];
 						// }
 						
-					// }else{
-						// $qtycount = 0;
-						
 					// }
 
 						$statement1 = $connec->query("insert into m_piline (m_pi_key, ad_org_id, isactived, insertdate, insertby, postdate, m_storage_id, m_product_id, sku, qtyerp, qtycount, qtysales, price, status, qtysalesout, status1, barcode, hargabeli) 
@@ -2368,7 +2365,7 @@ if($_GET['modul'] == 'inventory'){
 			// (m_piline.sku like '%".$sku."%' or LOWER(pos_mproduct.name) like LOWER('%".$sku."%')) order by m_piline.insertdate desc limit 50";
 			
 			$list_line = "select distinct m_piline.insertdate, m_piline.m_piline_key, m_piline.barcode, m_piline.sku ,m_piline.qtyerp, m_piline.qtycount, pos_mproduct.name, m_pi.status from m_pi inner join m_piline on m_pi.m_pi_key = m_piline.m_pi_key left join pos_mproduct on m_piline.sku = pos_mproduct.sku where m_pi.status = '1' and 
-			(m_piline.sku like '%".$sku."%' or LOWER(pos_mproduct.name) like LOWER('%".$sku."%')) order by m_piline.insertdate desc limit 50";
+			(m_piline.sku like '%".$sku."%' or m_piline.barcode like '%".$sku."%' or LOWER(pos_mproduct.name) like LOWER('%".$sku."%')) order by m_piline.insertdate desc limit 50";
 			
 		}else{
 			
