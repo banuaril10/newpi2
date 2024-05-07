4ed428bf10e5e3204deed13f58aa5ac531134460
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-05-07 13:12:59 +0700

    2024-05-07

--- api/action.php
+++ api/action.php
@@ -2239,7 +2239,7 @@ if($_GET['modul'] == 'inventory'){
 		if($sku != ""){
 			
 			$list_line = "select distinct m_piline.insertdate, m_piline.m_piline_key, m_piline.barcode, m_piline.sku ,m_piline.qtyerp, m_piline.qtycount, pos_mproduct.name, m_pi.status from m_pi inner join m_piline on m_pi.m_pi_key = m_piline.m_pi_key left join pos_mproduct on m_piline.sku = pos_mproduct.sku where m_pi.m_pi_key = '".$_GET['m_pi']."' and m_pi.status = '1' and 
-			(m_piline.sku like '%".$sku."%' LOWER(pos_mproduct.name) like LOWER('%".$sku."%')) order by m_piline.insertdate desc limit 50";
+			(m_piline.sku like '%".$sku."%' or LOWER(pos_mproduct.name) like LOWER('%".$sku."%')) order by m_piline.insertdate desc limit 50";
 			
 		}else{
 			
