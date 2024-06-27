bf34f9b9f41d4a86f8a5c9d880a1485d660c0372
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-06-27 15:16:58 +0700

    25-06-2024

--- api/cek_promo_active_sku.php
+++ api/cek_promo_active_sku.php
@@ -28,14 +28,20 @@ $discount_name = "";
 		$nama_product = $r1['name'];
 	}
 	
-	$cek_grosir = "select discount, discountname from pos_mproductdiscountgrosir_new where sku = '".$sku."' and DATE(now()) between fromdate and todate order by minbuy asc";
+	$cek_reguler = "select discount, discountname from pos_mproductdiscount where sku = '".$sku."' and DATE(now()) between fromdate and todate ";
+	$cr = $connec->query($cek_reguler);
+	foreach ($cr as $r1){
+		$discount_name .= $r1['discountname'].', Potongan '.rupiah($r1['discount']).'/Pcs <br>';
+	}
+	
+	$cek_grosir = "select discount, discountname from pos_mproductdiscountgrosir_new where sku = '".$sku."' and DATE(now()) between fromdate and todate and minbuy > 1 order by minbuy asc";
 	$cv = $connec->query($cek_grosir);
 	foreach ($cv as $r1){
 		$discount_name .= $r1['discountname'].', Potongan '.rupiah($r1['discount']).'/Pcs <br>';
-		$info = $nama_product.'<br> '.$discount_name.'';
+		
 	}
 	
-	
+	$info = $nama_product.'<br> '.$discount_name.'';
 	
 	// $info = $nama_product.'<br> '.$discount_name.'';
 // }
