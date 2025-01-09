58eb9b48b5304b9a88102a16b5f30a999d87d7ff
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-01-09 09:26:57 +0700

    push

--- report_sales_item_today.php
+++ report_sales_item_today.php
@@ -86,27 +86,29 @@
 <script type="text/javascript">
 	//create datatable sumber data from json api/cyber/report_sales_item.php
 	$(document).ready(function() {
-		$('#example').DataTable({
-			"ajax": "api/cyber/report_sales_item.php",
-			"columns": [
-				{ "data": "no" },
-				{ "data": "sku" },
-				{ "data": "name" },
-				{ "data": "qty" },
-				{ "data": "amount" }
-			],
-			"columnDefs": [ {
-				"searchable": false,
-				"orderable": false,
-				"targets": 0
-			} ],
-			"order": [[ 1, 'asc' ]]
+
+			$('#example').DataTable({
+			"ajax": {
+				"url": "api/cyber/report_sales_item.php",
+				"dataSrc": ""
+			},
+			"columns": [{
+					"data": "no"
+				},
+				{
+					"data": "sku"
+				},
+				{
+					"data": "name"
+				},
+				{
+					"data": "qty"
+				},
+				{
+					"data": "amount"
+				}
+			]
 		});
-		$('#example').on( 'order.dt search.dt', function () {
-			$('#example').DataTable().column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
-				cell.innerHTML = i+1;
-			});
-		}).draw();
 	});
 
 
