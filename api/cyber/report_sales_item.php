319e6c8f437b6354abc2662fde22e027c9f13868
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-01-09 09:45:53 +0700

    push

--- api/cyber/report_sales_item.php
+++ api/cyber/report_sales_item.php
@@ -3,7 +3,7 @@
 
 $tgl_filter = $_GET['date'];
 
-$query = "SELECT date(a.insertdate) date, a.sku, b.name, sum(a.qty) qty, sum(a.amount) amount, sum(b.price) price FROM pos_dsalesline a 
+$query = "SELECT date(a.insertdate) date, a.sku, b.name, sum(a.qty) qty, sum(a.price * a.qty) amount FROM pos_dsalesline a 
 left join pos_mproduct b on a.sku = b.sku where a.sku != '' ";
 
 
@@ -17,7 +17,7 @@ if ($tgl_filter != "") {
 $query .= ' group by date(a.insertdate), a.sku, b.name
 order by date(a.insertdate) desc';
 
-echo $query;
+// echo $query;
 
 
 $json = array();
@@ -29,7 +29,7 @@ foreach ($statement as $r) {
     $sku = $r['sku'];
     $name = $r['name'];
     $qty = $r['qty'];
-    $amount = $r['qty'] * $r['price'];
+    $amount = $r['amount'];
 
     $json[] = array(
         "no" => $no,
