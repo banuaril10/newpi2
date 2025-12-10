a300e4842ace9b39ec21d5c733d96f535fc7fa3f
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-12-10 13:28:17 +0700

    09-12-2025

--- create_table.php
+++ create_table.php
@@ -979,6 +979,16 @@ foreach ($indexing_table_promo_new as $r) {
 	$connec->exec($r);
 }
 
+//ALTER TABLE m_pi ADD COLUMN length VARCHAR(50);
+
+$cmd_alter_length = ['ALTER TABLE m_pi ADD COLUMN IF NOT EXISTS length VARCHAR(150);'];
+foreach ($cmd_alter_length as $r) {
+	$connec->exec($r);
+}
+
+
+
+
 
 ?>
 
